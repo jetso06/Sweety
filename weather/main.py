2bce07b2a1894257080cc68240b217b3320bec3c
jetso06 <69298123+jetso06@users.noreply.github.com>
2021-05-10 16:01:44 +0400

    Create main.py

---
+++ weather/main.py
@@ -0,0 +1,33 @@
+#MODULES
+import os
+from keep_alive import keep_alive
+import discord
+import requests
+import json
+from weather import *
+
+
+#STARTER
+api_key = 'f6ddca5ce386b72d0bf02bb4d0589b21'
+client = discord.Client()
+command_prefix = '$w'
+
+
+#MAIN
+@client.event
+async def on_message(message):
+    if message.author != client.user and message.content.startswith(command_prefix):
+        if len(message.content.replace(command_prefix, '')) >= 1:
+            location = message.content.replace(command_prefix, '').lower()
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=imperial'
+            try:
+                data = parse_data(json.loads(requests.get(url).content)['main'])
+                await message.channel.send(embed=weather_message(data, location))
+            except KeyError:
+                await message.channel.send(embed=error_message(location))
+
+
+#SHUSH
+keep_alive()
+my_secret = os.environ['token']
+client.run(my_secret)
